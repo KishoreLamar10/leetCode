7470a2c90686658653ec9a2b8c1778fdbb739009
KishoreLamar10 <30746601+KishoreLamar10@users.noreply.github.com>
2025-10-28 16:12:49 -0400

    [875] [Time Beats: 34.48%] [Memory Beats: 9.08%] - LeetPush

---
+++ Koko-Eating-Bananas.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def minEatingSpeed(self, piles, h):
+        """
+        :type piles: List[int]
+        :type h: int
+        :rtype: int
+        """
+        l,r = 1, max(piles)
+
+        res = r
+
+        while l<=r:
+            m = (l+r) // 2
+            time = 0
+
+            for p in piles:
+                time += math.ceil(float(p) / m)
+            
+            if time <= h:
+                res = m
+                r = m-1
+            else:
+                l = m+1
+        
+        return res
+        
