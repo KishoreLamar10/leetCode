68e597354b17934931660199bf45e8899cb5acb9
Kishorekumar Avudai Nayagam <30746601+KishoreLamar10@users.noreply.github.com>
2026-06-18 11:28:24 -0400

    [235] [Time Beats: 52.58%] [Memory Beats: 83.77%] - LeetPush

--- Lowest-Common-Ancestor-of-a-Binary-Search-Tree.py
+++ Lowest-Common-Ancestor-of-a-Binary-Search-Tree.py
@@ -1,25 +1,24 @@
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
-class Solution(object):
-    def lowestCommonAncestor(self, root, p, q):
-        """
-        :type root: TreeNode
-        :type p: TreeNode
-        :type q: TreeNode
-        :rtype: TreeNode
-        """
-        cur = root
-
-        while cur:
-            if p.val > cur.val and q.val > cur.val:
-                cur = cur.right
-            elif p.val < cur.val and q.val < cur.val:
-                cur = cur.left
-            else:
-                return cur
-        
+1# Definition for a binary tree node.
+2# class TreeNode(object):
+3#     def __init__(self, x):
+4#         self.val = x
+5#         self.left = None
+6#         self.right = None
+7
+8class Solution(object):
+9    def lowestCommonAncestor(self, root, p, q):
+10        """
+11        :type root: TreeNode
+12        :type p: TreeNode
+13        :type q: TreeNode
+14        :rtype: TreeNode
+15        """
+16        cur = root
+17
+18        while cur:
+19            if p.val > cur.val and q.val > cur.val:
+20                cur = cur.right
+21            elif p.val < cur.val and q.val < cur.val:
+22                cur = cur.left
+23            else:
+24                return cur
