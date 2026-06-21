00d233a55ed71e23bdec973a42708f4eb5c921b9
Kishorekumar Avudai Nayagam <30746601+KishoreLamar10@users.noreply.github.com>
2026-06-21 10:04:25 -0400

    [1833] [Time Beats: 95.51%] [Memory Beats: 21.35%] - LeetPush

--- Maximum-Ice-Cream-Bars.py
+++ Maximum-Ice-Cream-Bars.py
@@ -5,18 +5,25 @@
 5        :type coins: int
 6        :rtype: int
 7        """
-8        xMin = xMax = max(costs)
-9        freq = [0] * (xMax + 1)
-10        for x in costs:
-11            freq[x] += 1
-12            xMin = min(xMin, x)
-13        cnt = 0
-14        for x, f in enumerate(freq[xMin:], start=xMin):
-15            if f == 0:
-16                continue
-17            buy = min(coins // x, f)
-18            if buy == 0:
-19                break
-20            cnt += buy
-21            coins -= buy * x
-22        return cnt
+8        max_cost = max(costs)
+9
+10        freq = [0] * (max_cost + 1)
+11
+12        for cost in costs:
+13            freq[cost] += 1
+14
+15        ans = 0
+16
+17        for price in range(1, max_cost + 1):
+18            if freq[price] == 0:
+19                continue
+20
+21            can_buy = min(freq[price], coins // price)
+22
+23            ans += can_buy
+24            coins -= can_buy * price
+25
+26            if coins < price:
+27                break
+28
+29        return ans
