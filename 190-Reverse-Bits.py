877eb15697fa61e277568d8dab331081a6bd7c96
Kishorekumar Avudai Nayagam <30746601+KishoreLamar10@users.noreply.github.com>
2026-08-26 11:35:12 -0400

    [190] [Time Beats: 37.59%] [Memory Beats: 53.33%] - LeetPush

---
+++ 190-Reverse-Bits.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def reverseBits(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        res = 0
+
+        for i in range(32):
+            bit = (n >> i) & 1
+            res += (bit << (31 - i))
+        return res
