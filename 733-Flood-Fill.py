5881499f9a1d6ff5054f631d34b8b7a38acba6b3
Kishorekumar Avudai Nayagam <30746601+KishoreLamar10@users.noreply.github.com>
2026-08-31 13:57:05 -0400

    [733] [Time Beats: 100.00%] [Memory Beats: 62.87%] - LeetPush

---
+++ 733-Flood-Fill.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def fill(self, image, sr, sc, color, cur):
+            if ( sc < 0 or sc >= len(image[0]) or sr < 0 or sr >= len(image)): return
+            if cur != image[sr][sc]: return
+            image[sr][sc] = color
+
+            self.fill(image, sr + 1 , sc, color, cur)
+            self.fill(image, sr - 1, sc, color, cur)
+            self.fill(image, sr, sc + 1, color, cur)
+            self.fill(image, sr, sc - 1, color, cur)
+        
+    def floodFill(self, image, sr, sc, color):
+        """
+        :type image: List[List[int]]
+        :type sr: int
+        :type sc: int
+        :type color: int
+        :rtype: List[List[int]]
+        """
+        if image[sr][sc] == color:
+            return image
+        self.fill(image,sr,sc,color,image[sr][sc])
+        return image
+        
