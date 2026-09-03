9260723657f1472aa69e879e85c5cf0bbd5fb74e
Kishorekumar Avudai Nayagam <30746601+KishoreLamar10@users.noreply.github.com>
2026-09-03 14:39:10 -0400

    [409] [Time Beats: 14.49%] [Memory Beats: 94.09%] - LeetPush

---
+++ 409-Longest-Palindrome.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        odd_count = 0
+        
+        d = {}
+        
+        for c in s:
+            if c in d:
+                d[c] += 1
+            else:
+                d[c] = 1
+            if d[c] % 2 == 1:
+                odd_count += 1
+            else:
+                odd_count -= 1
+        
+        if odd_count > 1:
+            return len(s) - odd_count + 1
+        else:
+            return len(s)
